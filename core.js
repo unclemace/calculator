6daa82bd96aa6d90f9e3143bafdf13cfa9b28a3e
Erik <unclebulawa@yandex.ru>
2020-03-05 23:05:40 +0300

    added keyboard render from config

--- core.js
+++ core.js
@@ -1,22 +1,23 @@
 const keys = {
-    'zero': { type: 'number', value: 0},
-    'one': { type: 'number', value: 1},
-    'two': { type: 'number',  value: 2},
-    'three': { type: 'number',  value: 3},
-    'four': { type: 'number',  value: 4},
-    'five': { type: 'number',  value: 5},
-    'six': { type: 'number',  value: 6},
-    'seven': { type: 'number',  value: 7},
-    'eight': { type: 'number',  value: 8},
-    'nine': { type: 'number',  value: 9},
-    'minus': { type: 'operator',value:'-'},
-    'plus': { type: 'operator',value:'+'},
-    'multiply': { type: 'operator',value:'*'},
-    'equal': { type: 'operator',value:'='},
-    'dot': { type: 'operator',value:'.'},
-    'plus-minus': { type: 'operator',value:'+-'},
-    'division':{type:'operator',value:'/'},
-    'clear':{type:'operator'}
+    'zero': { type: 'number', value: 0,rowId:0,position:0,doubleKey:true},
+    'one': { type: 'number', value: 1,rowId:1,position:0},
+    'two': { type: 'number',  value: 2,rowId:1,position:1},
+    'three': { type: 'number',  value: 3,rowId:1,position:2},
+    'four': { type: 'number',  value: 4,rowId:2,position:0},
+    'five': { type: 'number',  value: 5,rowId:2,position:1},
+    'six': { type: 'number',  value: 6,rowId:2,position:2},
+    'seven': { type: 'number',  value: 7,rowId:3,position:0},
+    'eight': { type: 'number',  value: 8,rowId:3,position:1},
+    'nine': { type: 'number',  value: 9,rowId:3,position:2},
+    'minus': { type: 'operator',value:'-',rowId:2,position:3},
+    'plus': { type: 'operator',value:'+',rowId:1,position:3},
+    'multiply': { type: 'operator',value:'*',rowId:3,position:3},
+    'equal': { type: 'operator',value:'=',rowId:0,position:2},
+    'dot': { type: 'operator',value:'.',rowId:0,position:1},
+    'plus-minus': { type: 'operator',value:'+-',rowId:4,position:1},
+    'division':{type:'operator',value:'/',rowId:4,position:3},
+    'percent':{type:'operator',value:'%',rowId:4,position:2},
+    'clear':{type:'operator',value:'c',rowId:4,position:0}
 }
 
 const state = {
@@ -38,11 +39,10 @@ function setVisibleNumber(value) {
     state.visibleNumber = value;
 }
 
-
+render(keys);
 const keyboard = document.querySelector('.keyboard');
 const viewField = document.querySelector('.view-field');
 setResult(0);
-
 keyboard.addEventListener('click', (e) => {
     const id = e.target.id;
     const keyConfig = keys[id];
@@ -128,5 +128,52 @@ function clearLastValue(){
             setVisibleNumber(0);
     }
     
+}
+
+function drawKeyBoard(){
+    const basic = document.querySelector('.basic');
+    return appendNamedDiv(basic,'keyboard');
+}
+
+function drawRow(keyboard) {
+    return appendNamedDiv(keyboard,'keyboard-row');
+}
+
+function drawButton(row, config,key) {
+    const button = appendNamedDiv(row,'key');
+    button.classList.add(`${config.type}-key`);
+    button.id = key;
+    button.innerHTML = config.value;
+    if(config.doubleKey){
+        button.classList.add('double-key');
+    }
+}
+
+function appendNamedDiv(parent,className) {
+    const element = document.createElement('div');
+    element.classList.add(className);
+    parent.appendChild(element);
+    return element;
+}
+
+function render(keys){
+    const keyboard = drawKeyBoard();
+    let rows = {};
+    const sortedKeys = Object.keys(keys).sort((a,b)=>{
+        if(keys[a].rowId === keys[b].rowId){
+            return keys[a].position-keys[b].position;
+
+        }
+        return keys[b].rowId-keys[a].rowId;
+    });
+    sortedKeys.forEach(key => {
+        const keyConfig = keys[key];
+        const rowId = keyConfig.rowId;
+        if(!rows[rowId]) {
+            rows[rowId] = drawRow(keyboard);
+        }
+        const row = rows[rowId];
+        drawButton(row, keyConfig, key);
+    })
 }
    
